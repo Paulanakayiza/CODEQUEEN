086b636faa7482c6f6bb8068b1849b0be61d2f87
Paulanakayiza <nigelpaula08@gmail.com>
2020-11-04 12:33:06 +0300

    newchanges

--- statements/forifloop.js
+++ statements/forifloop.js
@@ -1,110 +1,110 @@
 const students=[
     {id: 1,
-     Name: "Paula",
-     Age: 18,
-     Subjects:[
+     name: "Paula",
+     age: 18,
+     subjects:[
          {id: 1,
-         Name: "Math",
-         Score: 90,
+         name: "Math",
+         score: 90,
          }, 
         {id: 2,
-        Name: "English",
+        name: "English",
         score: 75,
         },
         {id: 3,
-        Name: "Literature",
-        Score: 32,}
+        name: "Literature",
+        score: 32,}
              ]
     },
     
     {id: 2,
-    Name: "Mark",
-    Age: 15,
-    Subjects:[
+    name: "Mark",
+    age: 15,
+    subjects:[
         {id: 1,
-        Name: "Math",
-        Score:55,
+        name: "Math",
+        score:55,
         },
         {id: 2,
-         Name:"English",
-         Score:79,
+         name:"English",
+         score:79,
         },
         {id: 3,
-         Name: "Literature",
-         Score: 50,
+         name: "Literature",
+         score: 50,
         },
             ]
     },
     
     {id: 3,
-     Name: "Paul",
-     Age: 19,
-     Subjects:[
+     name: "Paul",
+     age: 19,
+     subjects:[
          {id: 1,
-         Name: "Math",
-         Score: 60,
+         name: "Math",
+         score: 60,
          },
         {id: 2,
-        Name: "English",
+        name: "English",
         score: 55,
         },
         {id: 3,
-        Name: "Literature",
-        Score: 90,}
+        name: "Literature",
+        score: 90,}
               ]
     },
     
         {id: 4,
-     Name: "Jackson",
-     Age: 17,
-     Subjects:[
+     name: "Jackson",
+     age: 17,
+     subjects:[
          {id: 1,
-         Name: "Math",
-         Score: 40,
+         name: "Math",
+         score: 40,
          },
         {id: 2,
-        Name: "English",
+        name: "English",
         score: 86,
         },
         {id: 3,
-        Name: "Literature",
-        Score: 62,}
+        name: "Literature",
+        score: 62,}
               ]
     },
     
     {id: 5,
-     Name: "Sharon",
-     Age: 20,
-     Subjects:[
+     name: "Sharon",
+     age: 20,
+     subjects:[
          {id: 1,
-         Name: "Math",
-         Score: 70,
+         name: "Math",
+         score: 70,
          },
         {id: 2,
-        Name: "English",
+        name: "English",
         score: 85,
         },
         {id: 3,
-        Name: "Literature",
-        Score: 51,}
+        name: "Literature",
+        score: 51,}
               ]
     },
     
     {id: 6,
-     Name: "Luke",
-     Age: 25,
-     Subjects:[
+     name: "Luke",
+     age: 25,
+     subjects:[
          {id: 1,
-         Name: "Math",
-         Score: 62,
+         name: "Math",
+         score: 62,
          },
         {id: 2,
-        Name: "English",
+        name: "English",
         score: 53,
         },
         {id: 3,
-        Name: "Literature",
-        Score: 68,}
+        name: "Literature",
+        score: 68,}
               ]
     },
     
@@ -114,21 +114,21 @@ let division= $("<div>", {class: "subDiv"});
     $(".display").append(division);
     
     let stdId= $(`<p> ${student.id}</p>`);
-    let stdName= $(`<p> ${student.Name}</p>`);
-    let stdAge= $(`<p> ${student.Age}</p>`);
+    let stdName= $(`<p> ${student.name}</p>`);
+    let stdAge= $(`<p> ${student.age}</p>`);
     
-    $("division").append(stdId);
-    $("division").append(stdName);
-    $("division").append(stdAge);
+    $(division).append(stdId);
+    $(division).append(stdName);
+    $(division).append(stdAge);
     
-    for (let subject of student.Subjects){
-       let subName = $(`<p> ${Subjects.id}, ${Subjects.Name}</p>`);
-       let subscore = $(`<p> ${Subjects.score}</p>`);
+    for (let subject of student.subjects){
+       let subName = $(`<p> ${subject.id}, ${subject.name}</p>`);
+       let subscore = $(`<p> ${subject.score}</p>`);
         
        
-        $("division").append(subName);
-        $("division").append(subscore);
-        }
+        $(division).append(subName);
+        $(division).append(subscore);
+    }
 }
 
 
